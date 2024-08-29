23282be040444add4d6a5e4658ae9d4681e28ae2
juguenkuzu <juguenkuzu@gmail.com>
2024-08-29 10:58:42 -0400

    first commit

---
+++ src/main/java/com/ApitestdemoApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitestdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitestdemoApplication.class, args);
+	}
+
+}
