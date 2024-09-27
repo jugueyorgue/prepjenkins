c1e43bf931aea55da716c5146d4994c2bd95b987
juguenkuzu <juguenkuzu@gmail.com>
2024-09-27 13:32:51 -0400

    third

--- src/main/java/com/MyController.java
+++ src/main/java/com/MyController.java
@@ -8,7 +8,7 @@ public class MyController {
 	//http://localhost:8081/api/health
 	@RequestMapping("api/health")
 	public String sayHello() {
-		return "Hello, Health are good for Nkuzu family";
+		return "Hello Jugue Nkuzu";
 	}
 	
 }
